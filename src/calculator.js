0bcff170d7bb7dfc611f020d00e7e937000f988e
kvale17 <kevin.valencia24@gmail.com>
2024-02-28 12:25:29 -0800

    Add operate function

--- src/calculator.js
+++ src/calculator.js
@@ -13,3 +13,15 @@ function multiply(a, b) {
 function divide(a, b) {
     return a / b;
 }
+
+function operate(a, operator, b) {
+    //i.e. operate(6, '-', 4)
+
+    switch (operator) {
+        case '+': return add(a, b);
+        case '-': return subtract(a, b);
+        case '*': return multiply(a, b);
+        case '/': return divide(a, b);
+        default: console.log("Invalid operator");
+    }
+}
